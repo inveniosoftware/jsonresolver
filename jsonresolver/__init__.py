e9b73e423e97957bd3c57e3bd8ff80001416d18a
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-01-27 12:31:11 +0100

    release: v0.5.0

--- jsonresolver/__init__.py
+++ jsonresolver/__init__.py
@@ -2,7 +2,7 @@
 #
 # This file is part of jsonresolver
 # Copyright (C) 2015 CERN.
-# Copyright (C) 2025 Graz University of Technology.
+# Copyright (C) 2025-2026 Graz University of Technology.
 #
 # jsonresolver is free software; you can redistribute it and/or
 # modify it under the terms of the Revised BSD License; see LICENSE
@@ -14,7 +14,7 @@ from .core import JSONResolver
 from .decorators import route
 from .hookimpl import hookimpl
 
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 
 __all__ = (
     "JSONResolver",
