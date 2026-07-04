4b80e643bc538e78a49cc2b495fd8bb91b1e963a
Maximilian Moser <maximilian.moser@tuwien.ac.at>
2026-07-05 00:09:17 +0200

    chore(setup): migrate from setuptools to hatchling
    * remove MANIFEST.in because that's only relevant for setuptools
    * format pyproject.toml with Tombi
    
    Co-authored-by: Sam Arbid <samaa2@kth.se>

--- setup.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2015, 2016 CERN.
-# SPDX-FileCopyrightText: 2022 Graz University of Technology.
-# SPDX-License-Identifier: BSD-3-Clause
-
-"""JSON data resolver with support for plugins."""
-
-from setuptools import setup
-
-setup()
